d272d3b1d94c559fb5fc34273e6d2ceb0b0027e7
kamilla <kamilla@gerlionti.com>
2020-10-30 07:46:21 +0200

    users

--- src/UsersPage/UsersPage.jsx
+++ src/UsersPage/UsersPage.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { userActions } from "../_actions";
+import { Cards } from "../_components";
 
 export const UsersPage = () => {
+  const users = useSelector((state) => state.users);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(userActions.getAll());
+  }, []);
+
   return (
-    <div>users</div>
+    <>
+      {users.loading && <p>Loading users...</p>}
+      {users.error && <p className="text-danger">ERROR: {users.error}</p>}
+      {users.items && <Cards data={users.items.data} />}
+    </>
   );
 }
